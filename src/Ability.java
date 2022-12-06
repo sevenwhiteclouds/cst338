9a21d36204a70ee89a827c6f9997d55b33027345
sevenwhiteclouds <kemaldonado@csumb.edu>
2022-12-06 11:29:55 -0800

    WK06HW00: Strategy Part 01/02

---
+++ src/Ability.java
@@ -0,0 +1,8 @@
+public interface Ability {
+  /*
+   * Author: Keldin Maldonado
+   * Date: 2022 -12 - 6
+   * Abstract: WK06HW00: Strategy Part 01/02
+   * This is a tagging interface
+   */
+}
---
+++ src/MeleeAttack.java
@@ -0,0 +1,23 @@
+public class MeleeAttack implements Attack {
+  /*
+   * Author: Keldin Maldonado
+   * Date: 2022 -12 - 6
+   * Abstract: WK06HW00: Strategy Part 01/02
+   * This is an implementation of Attack.java.
+   */
+
+  Monster attacker;
+
+  public MeleeAttack(Monster attacker) {
+    this.attacker = attacker;
+  }
+
+  @Override
+  public Integer attack(Monster monster) {
+    /* There will be more implementation changes in the future.
+     * For now, this will only return null and will not print anything.
+     */
+
+    return null;
+  }
+}
